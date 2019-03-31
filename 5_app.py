dd127153c71ee480bb04a94d9814e112a56a3f8d
Digi-labs <director@digi-labs.co.in>
2019-03-31 20:51:55 +0530

    first commit

---
+++ 5_app.py
@@ -0,0 +1,156 @@
+#Usage: python app.py
+import os
+ 
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug import secure_filename
+from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
+from keras.models import Sequential, load_model
+import numpy as np
+import argparse
+import imutils
+import cv2
+import time
+import tensorflow as tf
+import uuid
+import base64
+
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+from distutils.version import StrictVersion
+# module level variables ##############################################################################################
+
+FROZEN_INFERENCE_GRAPH_LOC = os.getcwd() + "/inference_graph/frozen_inference_graph.pb"
+LABELS_LOC = os.getcwd() + "/" + "labelmap.pbtxt"
+NUM_CLASSES = 2
+
+img_width, img_height = 150, 150
+
+UPLOAD_FOLDER = 'uploads'
+TEST_FOLDER = 'test'
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg','png'])
+
+def get_as_base64(url):
+    return base64.b64encode(requests.get(url).content)
+
+def mai(image_path):
+    print("starting program . . .")
+
+
+
+    # load a (frozen) TensorFlow model into memory
+    detection_graph = tf.Graph()
+    with detection_graph.as_default():
+        od_graph_def = tf.GraphDef()
+        with tf.gfile.GFile(FROZEN_INFERENCE_GRAPH_LOC, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+        # end with
+    # end with
+
+    # Loading label map
+    # Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
+    label_map = label_map_util.load_labelmap(LABELS_LOC)
+    categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,
+                                                                use_display_name=True)
+    category_index = label_map_util.create_category_index(categories)
+
+    with detection_graph.as_default():
+        with tf.Session(graph=detection_graph) as sess:
+           
+
+                print(image_path)
+
+                image_np = cv2.imread(image_path)
+                image_np = cv2.resize(image_np, (960, 640))
+
+                
+
+                # Definite input and output Tensors for detection_graph
+                image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+                # Each box represents a part of the image where a particular object was detected.
+                detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+                # Each score represent how level of confidence for each of the objects.
+                # Score is shown on the result image, together with the class label.
+                detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+                detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+                num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+                # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+                image_np_expanded = np.expand_dims(image_np, axis=0)
+                # Actual detection.
+                (boxes, scores, classes, num) = sess.run(
+                    [detection_boxes, detection_scores, detection_classes, num_detections],
+                    feed_dict={image_tensor: image_np_expanded})
+                # Visualization of the results of a detection.
+                vis_util.visualize_boxes_and_labels_on_image_array(image_np,
+                                                                   np.squeeze(boxes),
+                                                                   np.squeeze(classes).astype(np.int32),
+                                                                   np.squeeze(scores),
+                                                                   category_index,
+                                                                   use_normalized_coordinates=True,
+                                                                   line_thickness=8)
+                cv2.imshow("image_np", image_np)
+                cv2.imwrite('test/image.jpg', image_np)
+                cv2.waitKey()
+            # end for
+        # end with
+    # end with
+# end main
+
+def my_random_string(string_length=10):
+    """Returns a random string of length string_length."""
+    random = str(uuid.uuid4()) # Convert UUID format to a Python string.
+    random = random.upper() # Make all characters uppercase.
+    random = random.replace("-","") # Remove the UUID '-'.
+    return random[0:string_length] # Return the random string.
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route("/")
+def template_test():
+    return render_template('template.html', imagesource='../uploads/template.jpg', imagesource2="../uploads/template.jpg")
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        import time
+        start_time = time.time()
+        file = request.files['file']
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            mai(file_path)
+            
+            print(file_path)
+            filename = my_random_string(6) + filename
+
+            os.rename(file_path, os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            print("--- %s seconds ---" % str (time.time() - start_time))
+            return render_template('template.html', imagesource='../uploads/' + filename, imagesource2='../test/image.jpg')
+
+from flask import send_from_directory
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+from werkzeug import SharedDataMiddleware
+app.add_url_rule('/uploads/<filename>', 'uploaded_file',
+                 build_only=True)
+app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+    '/uploads':  app.config['UPLOAD_FOLDER']
+})
+
+if __name__ == "__main__":
+    app.debug=False
+    app.run(host='0.0.0.0', port=3000)
